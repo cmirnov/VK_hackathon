fab677359bec3f78b18880e0f1acf4fe6feb9fc4
petr <petr@kulinar>
2017-10-21 16:45:03 +0300

    PageView

---
+++ PageView/app/src/main/java/com/example/petr/pageview/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.petr.pageview;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //=======================================================================================
+        // getting PageView
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+
+        // get TabLayout - dots
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
+
+        // binding PageView and dots
+        tabLayout.setupWithViewPager(pager, true);
+
+        // init adapter for manage fragments
+        PagerAdapter pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+
+        // binding adapter and PageView
+        pager.setAdapter(pagerAdapter);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d("myLog", "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        //=======================================================================================
+    }
+
+
+    //=======================================================================================
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+    }
+    //=======================================================================================
+
+
+}
---
+++ PageView/app/src/main/java/com/example/petr/pageview/PageFragment.java
@@ -0,0 +1,44 @@
+package com.example.petr.pageview;
+
+import java.util.Random;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class PageFragment extends Fragment {
+
+    int pageNumber;
+    int backColor;
+
+    static PageFragment newInstance(int page) {
+        PageFragment pageFragment = new PageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt("arg_page_number", page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt("arg_page_number");
+        Random rnd = new Random();
+        backColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null);
+
+        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+        tvPage.setText("Page " + pageNumber);
+        tvPage.setBackgroundColor(backColor);
+
+        return view;
+    }
+}
